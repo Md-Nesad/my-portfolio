49d7c985a273d3587b32c3d131d44ec4c74cc28b
Md. Nesad <131846011+Md-Nesad@users.noreply.github.com>
2025-06-30 22:48:37 +0600

    Home page responsive done after changing the website

--- components/share.jsx
+++ components/share.jsx
@@ -5,60 +5,60 @@ import {
   Linkedin,
   Twitter,
   Youtube,
-} from "lucide-react";
-import Link from "next/link";
-import React from "react";
+} from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
 
 export default function Share() {
   return (
-    <div className="flex gap-6 max-sm:gap-5 text-[#F59E18]">
+    <div className='flex gap-6 max-sm:gap-5 text-[#F59E18] max-sm:place-content-center'>
       <Link
-        href={"https://www.linkedin.com/in/md-nesad-30b5bb286/"}
-        target="blank"
-        className="hover:scale-125 transition hover:bg-[#127A88] bg-[#F59E18] text-white rounded-full p-2"
+        href={'https://www.linkedin.com/in/md-nesad-30b5bb286/'}
+        target='blank'
+        className='hover:scale-125 transition hover:bg-[#127A88] bg-[#F59E18] text-white rounded-full p-2'
       >
         <Linkedin />
       </Link>
 
       <Link
-        href={"https://www.facebook.com/web.developer.161242/"}
-        target="blank"
-        className="hover:scale-125 transition hover:bg-[#127A88] bg-[#F59E18] text-white rounded-full p-2"
+        href={'https://www.facebook.com/web.developer.161242/'}
+        target='blank'
+        className='hover:scale-125 transition hover:bg-[#127A88] bg-[#F59E18] text-white rounded-full p-2'
       >
         <Facebook />
       </Link>
 
       <Link
-        href={"https://x.com/MdNesad3023"}
-        target="blank"
-        className="hover:scale-125 transition hover:bg-[#127A88] bg-[#F59E18] text-white rounded-full p-2"
+        href={'https://x.com/MdNesad3023'}
+        target='blank'
+        className='hover:scale-125 transition hover:bg-[#127A88] bg-[#F59E18] text-white rounded-full p-2'
       >
         <Twitter />
       </Link>
 
       <Link
-        href={"https://www.instagram.com/sahahamad87/"}
-        target="blank"
-        className="hover:scale-125 transition hover:bg-[#127A88] bg-[#F59E18] text-white rounded-full p-2"
+        href={'https://www.instagram.com/sahahamad87/'}
+        target='blank'
+        className='hover:scale-125 transition hover:bg-[#127A88] bg-[#F59E18] text-white rounded-full p-2'
       >
         <Instagram />
       </Link>
 
       <Link
-        href={"https://github.com/Md-Nesad"}
-        target="blank"
-        className="hover:scale-125 transition hover:bg-[#127A88] bg-[#F59E18] text-white rounded-full p-2"
+        href={'https://github.com/Md-Nesad'}
+        target='blank'
+        className='hover:scale-125 transition hover:bg-[#127A88] bg-[#F59E18] text-white rounded-full p-2'
       >
         <Github />
       </Link>
 
       <Link
-        href={"https://www.linkedin.com/in/md-nesad-30b5bb286/"}
-        target="blank"
-        className="hover:scale-125 transition hover:bg-[#127A88] bg-[#F59E18] text-white rounded-full p-2"
+        href={'https://www.linkedin.com/in/md-nesad-30b5bb286/'}
+        target='blank'
+        className='hover:scale-125 transition hover:bg-[#127A88] bg-[#F59E18] text-white rounded-full p-2'
       >
         <Youtube />
       </Link>
     </div>
-  );
+  )
 }
